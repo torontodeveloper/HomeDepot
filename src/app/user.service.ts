dd29a079b9659150f4401a020152715338349c89
Nag <nag@gmail.com>
2017-08-22 23:09:33 -0400

    error handling

--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -1,6 +1,7 @@
 import {Injectable} from '@angular/core';
 import {Http, Response} from '@angular/http';
 import 'rxjs';
+import {Observable} from 'rxjs/Rx';
 
 @Injectable()
 export class User {
@@ -8,6 +9,10 @@ export class User {
 
     }
     get(){
-        return this.http.get('https://randomuser.me/api/?inc=gender,name,nat&results=10').map((resp:Response)=>resp.json().results);
+        return this.http.get('https://randomuser.me/api/?inc=gender,name,nat&results=10').map((resp:Response)=>resp.json().results) .catch(this.handleError());
+    }
+    handleError(error:any){
+        console.error(error);
+        return Observable.throw(error.json().error||"Server Error")
     }
 }
